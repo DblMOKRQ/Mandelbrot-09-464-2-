a604f30adc7f18a63fc764470dd25ec672992eb3
emil <kazan.emil@mail.ru>
2026-05-03 21:16:42 +0300

    modified

--- src/ru/gr0946x/ui/FractalHistory.java
+++ src/ru/gr0946x/ui/FractalHistory.java
@@ -6,4 +6,17 @@ public class FrastalHistory{
     private final LinkedList<FractaleState> states = new LinkedList<>();
     private final int maxSteps = 100;
 
+    //больше ли одного состояния
+    public boolean canUndo() {
+        return states.size() > 1;
+    }
+
+    //добавляем состояние
+    public void add(FractaleState state) {
+        if (states.size() >= maxSteps) {
+            states.removeFirst(); 
+        }
+        states.addLast(state);
+    }
+
 }
