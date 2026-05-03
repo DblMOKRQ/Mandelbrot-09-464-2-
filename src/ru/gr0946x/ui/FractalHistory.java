b6a019dc01ccc4cd5eefa5a60a8dbeca4436bbb5
emil <kazan.emil@mail.ru>
2026-05-03 21:12:29 +0300

    add file about history fractals

---
+++ src/ru/gr0946x/ui/FractalHistory.java
@@ -0,0 +1,9 @@
+package ru.gr0946x.ui;
+import java.util.LinkedList;
+//история состояния фрактала
+public class FrastalHistory{
+
+    private final LinkedList<FractaleState> states = new LinkedList<>();
+    private final int maxSteps = 100;
+
+}
