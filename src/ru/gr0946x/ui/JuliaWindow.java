14adbd4468d2ba064e45b4f47e840bce76c8fe22
wmned <nerenick720@gmail.com>
2026-05-07 18:26:59 +0300

    пункт 9 саитгареев апдейт

--- src/ru/gr0946x/ui/JuliaWindow.java
+++ src/ru/gr0946x/ui/JuliaWindow.java
@@ -1,86 +1,42 @@
 package ru.gr0946x.ui;
 
 import ru.gr0946x.Converter;
-import ru.gr0946x.ui.painting.Painter;
+import ru.gr0946x.ui.fractals.ColorFunction;
+import ru.gr0946x.ui.fractals.DefaultColorScheme;
+import ru.gr0946x.ui.painting.FractalPainter;
 
 import javax.swing.*;
 import java.awt.*;
 
-import static java.lang.Math.*;
-
 public class JuliaWindow extends JFrame {
 
     public JuliaWindow(double cx, double cy) {
-        setTitle("Julia set: c = " + cx + " + " + cy + "i");
+        setTitle("Множество Жюлиа: c = " + cx + " + " + cy + "i");
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setSize(600, 600);
         setLocationRelativeTo(null);
 
         var conv = new Converter(-2.0, 2.0, -2.0, 2.0);
 
-        Painter painter = new Painter() {
+        // Для Жюлиа пока ставим стандартный цвет
+        ColorFunction defaultColor = new DefaultColorScheme();
 
-            @Override
-            public int getWidth() {
-                return getContentPane().getWidth();
+        // ПУНКТ 9: Передаем функцию вычисления Жюлиа через лямбда-выражение!
+        FractalPainter painter = new FractalPainter((x, y) -> {
+            int maxIter = 200;
+            int iter = 0;
+            double zx = x;
+            double zy = y;
+
+            while (zx * zx + zy * zy < 4 && iter < maxIter) {
+                double newX = zx * zx - zy * zy + cx;
+                double newY = 2 * zx * zy + cy;
+                zx = newX;
+                zy = newY;
+                iter++;
             }
-
-            @Override
-            public int getHeight() {
-                return getContentPane().getHeight();
-            }
-
-            @Override
-            public void setWidth(int width) {}
-
-            @Override
-            public void setHeight(int height) {}
-
-            @Override
-            public void paint(Graphics g) {
-
-                conv.setXShape(-2.0, 2.0);
-                conv.setYShape(-2.0, 2.0);
-
-                int width = getContentPane().getWidth();
-                int height = getContentPane().getHeight();
-
-                for (int x = 0; x < getWidth(); x++) {
-                    for (int y = 0; y < getHeight(); y++) {
-
-                        double zx = conv.xScr2Crt(x);
-                        double zy = conv.yScr2Crt(y);
-
-                        int maxIter = 200;
-                        int iter = 0;
-
-                        while (zx * zx + zy * zy < 4 && iter < maxIter) {
-                            double newX = zx * zx - zy * zy + cx;
-                            double newY = 2 * zx * zy + cy;
-
-                            zx = newX;
-                            zy = newY;
-                            iter++;
-                        }
-
-                        double value = iter / (double) maxIter;
-
-                        Color color;
-                        if (iter == maxIter) {
-                            color = Color.BLACK;
-                        } else {
-                            float r = (float) abs(sin(5 * value));
-                            float gr = (float) abs(cos(8 * value) * sin(3 * value));
-                            float b = (float) abs((sin(7 * value) + cos(15 * value)) / 2f);
-                            color = new Color(r, gr, b);
-                        }
-
-                        g.setColor(color);
-                        g.drawLine(x, y, x, y);
-                    }
-                }
-            }
-        };
+            return (float) iter / maxIter;
+        }, conv, defaultColor);
 
         var panel = new PaintPanel(painter);
         panel.setBackground(Color.WHITE);
