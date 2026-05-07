d4c4ff6ee2edcba1e1fedb76ecafed6cc5d0fe95
SERGEYPOGOSYAN <sergey.pogosyan2006@gmail.com>
2026-05-07 17:43:51 +0300

    10 PUNKT SERGEY POGOSYAN last

--- src/ru/gr0946x/ui/fractals/Mandelbrot.java
+++ src/ru/gr0946x/ui/fractals/Mandelbrot.java
@@ -16,9 +16,6 @@ public class Mandelbrot implements Fractal{
         this.maxIterations = max(50, n);
     }
 
-    public int getMaxIterations() {
-        return maxIterations;
-    }
 
     @Override
     public float inSetProbability(double x, double y) {
